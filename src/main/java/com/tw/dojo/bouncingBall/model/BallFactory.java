b7652864a7409392a33ff81146a445a99a3648a0
Megha Jagadale <megha.jagadale@thoughtworks.com>
2022-07-15 17:14:19 +0530

    Added.Bouncing ball dojo game

--- src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
+++ src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
@@ -6,8 +6,13 @@ public class BallFactory {
         return new Ball[]{
                 bouncingBall(75, 50, BouncingBall.DOWN),
                 elasticBall(250, 100, Ball.DEFAULT_RADIUS, ElasticBall.SHRINK),
+                elasticBouncingBall(390, 100, ElasticBouncingBall.DOWN, ElasticBouncingBall.SHRINK, Ball.DEFAULT_RADIUS),
                 // bouncingElasticBall() --> Let's make a new ball!
         };
+    }
+
+    private static Ball elasticBouncingBall(int centerX, int centerY, int direction,int growthDirection, int radius) {
+        return new ElasticBouncingBall(centerX, centerY, direction, radius);
     }
 
     public static Ball bouncingBall(int centerX, int centerY, int direction) {
